54fbd480455be162a46cfae52dacf29f5063d307
Scarlet-Lotus <mamlukbeibarys@gmail.com>
2025-06-05 01:23:32 +0500

    Changed test

--- src/test/java/org/travel/insurance/TravelInsuranceApplicationTest.java
+++ src/test/java/org/travel/insurance/TravelInsuranceApplicationTest.java
@@ -1,14 +1,5 @@
 package org.travel.insurance;
 
-import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
 class TravelInsuranceApplicationTest {
-
-    @Test
-    public void test() {
-        assertTrue(true);
-    }
 
 }
